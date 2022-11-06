02d861aa8eaf5497577977852c2ada0e78fde614
Enzo Bognar <110871103+EnzoBognar@users.noreply.github.com>
2022-11-06 18:29:43 -0300

    Add files via upload

---
+++ Exercício 1 31 10 Recursividade.c
@@ -0,0 +1,16 @@
+//Enzo Bognar - RA 8123357 - Ciência da Computação 2º Semestre
+
+#include <stdio.h>
+
+void ciclo (int n, int limite) {
+
+       for (int i=n; i<= limite; i++) {
+    printf("%d\n", i);
+       }
+}
+
+int main()
+{
+    ciclo(2,6);
+}
+
